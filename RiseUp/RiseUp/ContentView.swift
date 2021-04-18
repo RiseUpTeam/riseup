d663d67de8ef66a85feabfb4df6dc243ce23035c
Michael Rodgers <mrodgers.chicago@gmail.com>
2021-04-18 07:45:19 -0500

    First screen

--- RiseUp/RiseUp/ContentView.swift
+++ RiseUp/RiseUp/ContentView.swift
@@ -7,10 +7,43 @@
 
 import SwiftUI
 
+
+struct StartApp: View {
+    var body: some View {
+        Text("Here we go")
+    }
+}
+
+struct StartApp_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image("Rise Up")
+                    .resizable().scaledToFit()
+                Spacer()
+                NavigationLink(
+                    destination: StartApp(),
+                    label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 50)
+                                .frame(width: 250, height: 50)
+                                .foregroundColor(.green)
+                            Text("Let's Get Started")
+                                .font(.title)
+                                .fontWeight(.heavy)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        }
+                    })
+                Spacer()
+            }
+        }
     }
 }
 
