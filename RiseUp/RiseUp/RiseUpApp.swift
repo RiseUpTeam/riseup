bfb1f299c67e6a78e6c549223f366914fc7471f2
Michael Rodgers <mrodgers.chicago@gmail.com>
2021-04-18 06:53:34 -0500

    icons

---
+++ RiseUp/RiseUp/RiseUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseUpApp.swift
+//  RiseUp
+//
+//  Created by Michael Rodgers on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RiseUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
