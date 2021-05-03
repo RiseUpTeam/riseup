da4726c1cdedd1df382be290423f858fac33937b
Michael Rodgers <mrodgers.chicago@gmail.com>
2021-05-02 22:30:51 -0500

    breathing and symptoms

--- RiseUp/RiseUp/BreathingView.swift
+++ RiseUp/RiseUp/BreathingView.swift
@@ -2,72 +2,59 @@
 //  BreathingView.swift
 //  RiseUp
 //
-//  Created by Michael Rodgers on 4/24/21.
+//  Created by Channice Tolbert on 4/24/21.
 //
 
 import SwiftUI
 import AppDevWithSwiftLibrary
 
-
 struct BreathingView: View {
-    var body: some View {
-        Loading()
-        
-    }
-}
-
-struct Loading : View {
     @State var fadeInOut = false
-    
-    @State var animate = false
+    @State var animate = true
+    @State var s = CGFloat(0)
     var body : some View {
-        
-        
-        ZStack{
-            
-            
-            Circle().fill(Color . green.opacity(0.25)).frame(width: 350, height: 350).scaleEffect(self.animate ? 1 : 0)
-            
-            
-            Circle().fill(Color . blue.opacity(0.35)).frame(width: 250, height: 250).scaleEffect(self.animate ? 1 : 0)
-            
-            
-            Circle().fill(Color . green.opacity(0.45)).frame(width: 150, height: 150).scaleEffect(self.animate ? 1 : 0)
-            
-            
-            Circle().fill(Color . blue).frame(width: 30, height: 30).scaleEffect(self.animate ? 1 : 0)
-            
-            
-            
-            
-        }.onAppear(){
-            
-            
-            self.animate.toggle()
-            
-            
-        }
-        .animation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true))
-        VStack{
+        VStack {
+            Spacer()
+            ZStack{
+                Circle()
+                    .fill(Color .green.opacity(0.25))
+                    .frame(width: animate ? 0 : 350)
+                Circle()
+                    .fill(Color .blue.opacity(0.35))
+                    .frame(width: animate ? 0 : 250)
+                Circle()
+                    .fill(Color .green.opacity(0.45))
+                    .frame(width: animate ? 0 : 150)
+                Circle()
+                    .fill(Color .blue)
+                    .frame(width: animate ? 0 : 50)
+            }.onAppear(){self.animate.toggle()}
+            .animation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true))
             Text("Inhale")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.blue)
+                .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .onAppear(){
                     withAnimation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true)) {
                         fadeInOut.toggle()
                     }
                 } .opacity(fadeInOut ? 0 : 1)
             Text("Exhale")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.blue)
+                .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .padding(.bottom,50)
+                .onAppear(){
+                    withAnimation(Animation.easeInOut(duration: 3.0).repeatForever(autoreverses: true)) {
+                        fadeInOut.toggle()
+                    }
+                } .opacity(fadeInOut ? 1 : 0)
             
-            
+            Spacer()
         }
-        
-        
-        
-        
-        
-        
     }
-    
-    
 }
 
 
--- RiseUp/RiseUp/CheckInSymptomsView.swift
+++ RiseUp/RiseUp/CheckInSymptomsView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct CheckInSymptomsView: View {
+    @State var level: Double = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Text("Excessive Worrying")
+            Text("Difficulty Sleeping")
+            Text("Fatigue/Dizzy")
+            Text("Sweating/Hot Flashes")
+            Text("Shortness of Breath")
+            Text("Chest Pain")
+            Text("Concentration Issues")
+            Text("Irritated")
+            Text("Feeling of Terror")
+            VStack {
+                Text("What Level are the Symptoms?")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("1")
+                    Spacer()
+                    Text("10")
+                } .foregroundColor(.blue)
+                Slider(value: $level, in: 0.0...10.0)
+                    .padding(.horizontal, 10.0)
+                
+                HStack {
+                    Text ("Level: " + String(Int(level)))
+                        .foregroundColor(.green)
+                }
+                .padding(.horizontal, 4.0)
+            }
+        }
     }
 }
 
